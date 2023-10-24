9adca93b69645f915398497b3d755a7b392168a3
Santiago <santiago.pilamunga@udla.edu.ec>
2023-10-24 14:43:42 -0500

    Agregamos una funcion

--- Examen.c
+++ Examen.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 int main()
 {
     int num;
     printf("Ingrese un numero entero: \n");
     scanf("%d", &num);
     return 0;
+}
+int suma(int a){
+    int digito,suma=0;
+    while (a!=0)
+    
+    {
+        digito=a%10;
+        a=trunc(a/10);
+        suma+=digito;
+        
+    }
+    
+        return suma;
 }
