d14333ac82f9d42e4da49c278172d095fcf0eefd
Santiago <santiago.pilamunga@udla.edu.ec>
2023-10-24 14:55:11 -0500

    Correcciones

--- Examen.c
+++ Examen.c
@@ -2,16 +2,17 @@
 #include <math.h>
 int main()
 {
-    int num;
+    int num,resultado;
     printf("Ingrese un numero entero: \n");
     scanf("%d", &num);
-     int resultado=suma(num);
+    resultado=suma(num);
+    printf("La suma es: %d \n",resultado);
     return 0;
 }
-int suma(int a){
+int suma(int a)
+{
     int digito,suma=0;
     while (a!=0)
-    
     {
         digito=a%10;
         a=trunc(a/10);
@@ -22,8 +23,7 @@ int suma(int a){
         }else{
             suma=suma-digito;
         }
-        printf("SUMA: %d \n",suma);
     }
     
-        return suma;
+    return suma;
 }
