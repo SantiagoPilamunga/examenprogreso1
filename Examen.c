bc20ecdd5e377e3f4940757beae2b67a1e5985d9
Santiago <santiago.pilamunga@udla.edu.ec>
2023-10-24 14:47:26 -0500

    Realizamos las operaciones

--- Examen.c
+++ Examen.c
@@ -5,6 +5,7 @@ int main()
     int num;
     printf("Ingrese un numero entero: \n");
     scanf("%d", &num);
+     int resultado=suma(num);
     return 0;
 }
 int suma(int a){
@@ -15,7 +16,13 @@ int suma(int a){
         digito=a%10;
         a=trunc(a/10);
         suma+=digito;
-        
+        if (suma<10 && suma>=0)
+        {
+            suma=suma;
+        }else{
+            suma=suma-digito;
+        }
+        printf("SUMA: %d \n",suma);
     }
     
         return suma;
